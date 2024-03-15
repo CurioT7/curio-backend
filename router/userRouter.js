29f3e3361b55b602fa8dc3baf4c96a417819afc9
Ahmed Mohamed Dandash <101134041+AhmedDandash@users.noreply.github.com>
2024-03-15 22:09:54 +0200

    clean userRouter

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,13 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controller/Auth/userController");
+const friendsController = require("../controller/friends/userController");
 
 router.post("/auth/signup", userController.signUp);
-
-router.post("/friend", userController.friendRequest);
-
-router.post("/unfriend", userController.unFriendRequest);
-
-router.get("/me/friends/:username", userController.getUserInfo);
 
 module.exports = router;
