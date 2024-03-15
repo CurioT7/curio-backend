2a6bd0f2ea6688ca7fa0306c393a6e6fb3f05ee1
Ahmed Mohamed Dandash <101134041+AhmedDandash@users.noreply.github.com>
2024-03-15 22:09:22 +0200

    create subredditRouter

---
+++ router/subredditRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const  subredditsController = require("../controller/friends/subredditsController");
+
+router.post("/createSubreddit",subredditsController.createSub);
+
+  module.exports = router;
