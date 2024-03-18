4279ed2bd0c617555843fdb69fc47a1f9c99ace7
Hassan <htarek1011@gmail.com>
2024-03-18 04:09:53 +0200

    feat: Email verification status now is sent in getMe

--- controller/identity/identityController.js
+++ controller/identity/identityController.js
@@ -5,9 +5,8 @@
 
 const User = require("../../models/user");
 const UserPreferences = require("../../models/userPreferences");
-const {generateToken,verifyToken } = require("../../utils/tokens");
+const { generateToken, verifyToken } = require("../../utils/tokens");
 require("dotenv").config();
-
 
 /**
  * @description Fetches user information based on the provided username.
@@ -19,31 +18,29 @@ async function getMe(req, res) {
   const token = req.headers.authorization.split(" ")[1];
   const decoded = await verifyToken(token);
   if (!decoded) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: "Unauthorized" });
   }
   try {
-    const userExists = await User.findOne({ _id: decoded.userId }); 
+    const userExists = await User.findOne({ _id: decoded.userId });
     if (!userExists) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
     const response = {
-        username: userExists.username,
-        gender: userExists.gender ||'N/A',
-        language: userExists.language || 'N/A',
-        email: userExists.email || 'N/A' 
-      };
-  
-      res.json(response);
+      username: userExists.username,
+      gender: userExists.gender || "N/A",
+      language: userExists.language || "N/A",
+      email: userExists.email || "N/A",
+      isVerified: userExists.isVerified,
+    };
 
-    } 
-    catch (error) {
-         return res.status(500).json({ 
-            success: false,
-            message: error.message });
-     }
-    
-};
-
+    res.json(response);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
 
 /**
  * @description Fetches user preferences based on the provided username.
@@ -54,35 +51,36 @@ async function getUserPreferences(req, res) {
   const token = req.headers.authorization.split(" ")[1];
   const decoded = await verifyToken(token);
   if (!decoded) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: "Unauthorized" });
   }
   try {
-    const user = await User.findOne({ _id: decoded.userId});
+    const user = await User.findOne({ _id: decoded.userId });
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
-    const preferences = await UserPreferences.findOne({ username: user.username   })
-    .populate({
-      path: 'block.username', // Populate username details in blocked users
-      select: 'username' // Only select username from User model in blocked users
+    const preferences = await UserPreferences.findOne({
+      username: user.username,
     })
-    .populate({
-      path: 'mute.username', // Populate username details in muted users
-      select: 'username' // Only select username from User model in muted users
-    });
-    
-    
+      .populate({
+        path: "block.username", // Populate username details in blocked users
+        select: "username", // Only select username from User model in blocked users
+      })
+      .populate({
+        path: "mute.username", // Populate username details in muted users
+        select: "username", // Only select username from User model in muted users
+      });
+
     if (!preferences) {
-      return res.status(404).json({ message: 'User preferences not found' });
+      return res.status(404).json({ message: "User preferences not found" });
     }
 
     res.json(preferences);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
-};
+}
 
 /**
  * @description Updates user preferences based on the provided username and preferences data.
@@ -94,51 +92,51 @@ async function updateUserPreferences(req, res) {
   const token = req.headers.authorization.split(" ")[1];
   const decoded = await verifyToken(token);
   if (!decoded) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   const user = await User.findOne({ _id: decoded.userId });
   if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+    return res.status(404).json({ message: "User not found" });
   }
 
   const updateFields = {};
   const preferencesFields = [
-    'gender',
-    'language',
-    'locationCustomization',
-    'displayName',
-    'about',
-    'socialLinks',
-    'images',
-    'NSFW',
-    'allowFollow',
-    'contentVisibility',
-    'activeInCommunityVisibility',
-    'clearHistory',
-    'block',
-    'viewBlockedPeople',
-    'mute',
-    'viewMutedCommunities',
-    'adultContent',
-    'autoplayMedia',
-    'communityThemes',
-    'communityContentSort',
-    'globalContentView',
-    'rememberPerCommunity',
-    'openPostsInNewTab',
-    'mentions',
-    'comments',
-    'upvotes',
-    'replies',
-    'newFollowers',
-    'postsYouFollow',
-    'newFollowerEmail',
-    'chatRequestEmail',
-    'unsubscribeFromAllEmails'
+    "gender",
+    "language",
+    "locationCustomization",
+    "displayName",
+    "about",
+    "socialLinks",
+    "images",
+    "NSFW",
+    "allowFollow",
+    "contentVisibility",
+    "activeInCommunityVisibility",
+    "clearHistory",
+    "block",
+    "viewBlockedPeople",
+    "mute",
+    "viewMutedCommunities",
+    "adultContent",
+    "autoplayMedia",
+    "communityThemes",
+    "communityContentSort",
+    "globalContentView",
+    "rememberPerCommunity",
+    "openPostsInNewTab",
+    "mentions",
+    "comments",
+    "upvotes",
+    "replies",
+    "newFollowers",
+    "postsYouFollow",
+    "newFollowerEmail",
+    "chatRequestEmail",
+    "unsubscribeFromAllEmails",
   ];
 
-  preferencesFields.forEach(field => {
+  preferencesFields.forEach((field) => {
     if (req.body[field] !== undefined) {
       updateFields[field] = req.body[field];
     }
@@ -151,12 +149,11 @@ async function updateUserPreferences(req, res) {
       { new: true, upsert: true }
     );
 
-    res.json({ preferences, message: 'User preferences updated successfully' });
-
+    res.json({ preferences, message: "User preferences updated successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 }
 
-module.exports = { getMe, getUserPreferences, updateUserPreferences};
+module.exports = { getMe, getUserPreferences, updateUserPreferences };
