5a30114d3983cc169ccd964831bb227ef1823130
Ahmed Mohamed Dandash <101134041+AhmedDandash@users.noreply.github.com>
2024-03-18 15:38:46 +0200

    docs: add jsdoc to routers

--- router/friendsRoute.js
+++ router/friendsRoute.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const router = express.Router();
-
 const friendsController = require("../controller/friends/friendController");
 
+/**
+ * Route to follow a subreddit.
+ * @name POST /friend
+ * @function
+ * @memberof module:routes/friends
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.post("/friend", friendsController.followSubreddit);
+
+/**
+ * Route to unfollow a subreddit.
+ * @name POST /unfriend
+ * @function
+ * @memberof module:routes/friends
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.post("/unfriend", friendsController.unFollowSubreddit);
-router.post("/me/friends/:username",friendsController.friendRequest);
+
+/**
+ * Route to send a friend request.
+ * @name POST /me/friends/:username
+ * @function
+ * @memberof module:routes/friends
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.post("/me/friends/:username", friendsController.friendRequest);
+
+/**
+ * Route to cancel a friend request.
+ * @name DELETE /me/friends/:username
+ * @function
+ * @memberof module:routes/friends
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.delete("/me/friends/:username", friendsController.unFriendRequest);
+
+/**
+ * Route to get user information.
+ * @name GET /me/friends/:username
+ * @function
+ * @memberof module:routes/friends
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
 router.get("/me/friends/:username", friendsController.getUserInfo);
+
 module.exports = router;
--- router/subredditRouter.js
+++ router/subredditRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const subredditsController = require("../controller/friends/subredditsController");
 
-const  subredditsController = require("../controller/friends/subredditsController");
+/**
+ * Route to create a new subreddit.
+ * @name POST /subreddit/createSubreddit
+ * @function
+ * @memberof module:routes/subreddit
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.post("/createSubreddit", subredditsController.newSubreddit);
 
-router.post("/createSubreddit",subredditsController.createSub);
-
-  module.exports = router;
+module.exports = router;
