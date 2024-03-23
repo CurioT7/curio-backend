58cb2f208f022ce6cdabe90f6ff82f85fcde3c89
Ahmed Mohamed Dandash <101134041+AhmedDandash@users.noreply.github.com>
2024-03-24 01:48:53 +0200

    feat: get top views' post

--- controller/listing/listingController.js
+++ controller/listing/listingController.js
@@ -1,4 +1,6 @@
 const subredditModel = require("../../models/subredditModel");
+const Post = require("../../models/postModel");
+
 
 /**
  * Get a random post from a subreddit.
@@ -27,4 +29,45 @@ async function randomPost(req, res) {
   }
 }
 
-module.exports = { randomPost };
+
+/**
+ * Get the top-viewed posts from a subreddit, or a random post if there are no top-viewed posts.
+ * @async
+ * @param {string} subredditName - The name of the subreddit.
+ * @returns {Promise<Object>} - The top-viewed post or a random post.
+ */
+async function getTopPosts(req, res) {
+  try {
+    const subredditName = req.params.subreddit;
+    // Find the subreddit
+    const subreddit = await subredditModel.findOne({ name: subredditName });
+
+    if (!subreddit) {
+     return res
+       .status(404)
+       .json({ success: false, message: "Subreddit not found" });
+    }
+
+    // Find top-viewed posts sorted by views in descending order
+    const topPosts = await Post.find({ linkedSubreddit: subreddit._id })
+      .sort({ views: -1 })
+      .limit(1)
+      .lean(); // Using lean() to convert documents to plain JavaScript objects
+
+    if (topPosts.length > 0) {
+      // If top-viewed posts exist, increment views of the first post
+      const topPost = topPosts[0];
+      await Post.updateOne({ _id: topPost._id }, { $inc: { views: 1 } });
+      res.status(200).json({ success: true, post: topPost });
+    }
+  } catch (error) {
+     res
+       .status(400)
+       .json({ success: false, message: "Error getting top post" });
+  }
+}
+
+module.exports = {
+  randomPost,
+  getTopPosts,
+};
