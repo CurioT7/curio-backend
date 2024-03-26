e426df831241a72b457810415ebfbc414204fd23
Hassan <htarek1011@gmail.com>
2024-03-26 20:15:32 +0200

    feat: added tests for unhide Posts

--- tests/contentManagement.test.js
+++ tests/contentManagement.test.js
@@ -77,3 +77,68 @@ describe("hidePost function", () => {
     });
   });
 });
+
+describe("unhidePost function", () => {
+  let req, res, userFindOneMock, postFindOneMock, userSaveMock;
+
+  beforeEach(() => {
+    userFindOneMock = jest.spyOn(User, "findOne");
+    userSaveMock = jest.spyOn(User.prototype, "save");
+    postFindOneMock = jest.spyOn(Post, "findOne");
+
+    req = {
+      headers: {
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjAyM2Q4MDdmNzBkYjg4M2NjZjVhOTIiLCJpYXQiOjE3MTE0NzU0ODgsImV4cCI6MTcxMTU2MTg4OH0.Yvil4qLVPXSV7cB5RBhiki7hzqFreQIR8rEUICBqPaU", // Your token here
+      },
+      body: {
+        postId: "660227d61650ec9f41404c80", // Your post ID here
+      },
+    };
+
+    res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return 401 if token is missing or invalid", async () => {
+    verifyToken.mockResolvedValue(null); // Simulate token verification failure
+
+    await unhidePost(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: "Unauthorized" });
+  });
+
+  it("should return 404 if user is not found", async () => {
+    const payload = { userId: "userId123" };
+    verifyToken.mockResolvedValue(payload); // Simulate token verification success
+    userFindOneMock.mockResolvedValue(null); // Simulate user not found
+
+    await unhidePost(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      success: false,
+      message: "User not found",
+    });
+  });
+
+  it("should handle internal server error", async () => {
+    const payload = { userId: "userId123" };
+    verifyToken.mockResolvedValue(payload); // Simulate token verification success
+    userFindOneMock.mockRejectedValue(new Error("Database error")); // Simulate database error
+
+    await unhidePost(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      success: false,
+      message: "Internal server error",
+    });
+  });
+});
