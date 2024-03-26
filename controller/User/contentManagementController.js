f9939f266447d7380c1fa8890ac8f965a4e77d01
Hassan <htarek1011@gmail.com>
2024-03-26 05:25:02 +0200

    Feat: added hide and unhide posts

--- controller/User/contentManagementController.js
+++ controller/User/contentManagementController.js
@@ -1,1 +1,79 @@
+User = require("../../models/userModel");
+Post = require("../../models/postModel");
+const { verifyToken } = require("../../utils/tokens");
 
+async function hidePost(req, res) {
+  const token = req.headers.authorization.split(" ")[1];
+  const postId = req.body.postId;
+  const decoded = await verifyToken(token);
+  if (!decoded) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const user = await User.findOne({ _id: decoded.userId });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const post = await Post({ _id: postId });
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+    if (user.hiddenPosts.includes(postId)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Post already hidden" });
+    }
+    user.hiddenPosts.push(postId);
+    await user.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Post hidden successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+}
+
+async function unhidePost(req, res) {
+  const token = req.headers.authorization.split(" ")[1];
+  const postId = req.body.postId;
+  const decoded = await verifyToken(token);
+  if (!decoded) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const user = await User.findOne({ _id: decoded.userId });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const post = await Post({ _id: postId });
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+    if (!user.hiddenPosts.includes(postId)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Post not hidden" });
+    }
+    await user.hiddenPosts.pull(postId);
+    await user.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Post unhidden successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+}
+
+module.exports = { hidePost, unhidePost };
