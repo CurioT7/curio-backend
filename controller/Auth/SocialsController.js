86f4bebdb51dfe22a58c468eb957ed182028fe07
Hassan <htarek1011@gmail.com>
2024-04-13 16:42:45 +0200

    Fix: Connect with google

--- controller/Auth/SocialsController.js
+++ controller/Auth/SocialsController.js
@@ -118,40 +118,49 @@ async function connectWithGoogle(req, res) {
   if (!decoded) {
     return res.status(401).json({ success: false, message: "Unauthorized" });
   }
+
   const googleToken = req.body.token;
-  const { password } = req.body.password;
+  const password = req.body.password;
+
   try {
     const response = await verifyGoogleToken(googleToken);
     if (response.status === 200) {
       let user = await User.findOne({ googleId: response.data.user_id });
+
       if (user) {
-        res.status(400).json({
+        return res.status(400).json({
           success: false,
           message: "Google account already connected",
         });
       }
+
       user = await User.findOne({ _id: decoded.userId });
+
       if (!user) {
         return res
           .status(404)
           .json({ success: false, message: "User not found" });
       }
+
       const isMatch = await comparePassword(password, user.password);
+
       if (!isMatch) {
         return res
           .status(400)
           .json({ success: false, message: "Invalid password" });
       }
+
       user.googleId = response.data.user_id;
       await user.save();
-      res.status(200).json({
+
+      return res.status(200).json({
         success: true,
         message: "Google account connected successfully",
       });
     }
   } catch (err) {
     console.error("Error connecting account", err);
-    res
+    return res
       .status(500)
       .json({ success: false, message: "Error connecting account" });
   }
