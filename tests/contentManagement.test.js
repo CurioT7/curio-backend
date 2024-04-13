6647a1fa875a7f927b8371f697139f78322b3ca5
Hassan <htarek1011@gmail.com>
2024-04-13 14:55:49 -0700

    Feat: Content management tests

--- tests/contentManagement.test.js
+++ tests/contentManagement.test.js
@@ -3,8 +3,12 @@ const {
   unhidePost,
   save,
   unsave,
-  hidden
-} = require("../controller/User/ContentManagementController");
+  hidden,
+  saved_categories,
+  submit,
+  sharePost,
+  getPostLink,
+} = require("../controller/User/contentManagementController");
 const { verifyToken } = require("../utils/tokens");
 const User = require("../models/userModel");
 const Post = require("../models/postModel");
@@ -350,6 +354,107 @@ describe("hidden function", () => {
     userFindOneMock.mockResolvedValue(null); // Simulate user not found
 
     await hidden(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      success: false,
+      message: "User not found",
+    });
+  });
+});
+
+describe("saved_categories function", () => {
+  let req, res, userFindOneMock, userSaveMock;
+
+  beforeEach(() => {
+    userFindOneMock = jest.spyOn(User, "findOne");
+    userSaveMock = jest.spyOn(User.prototype, "save");
+
+    req = {
+      headers: {
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjAyM2Q4MDdmNzBkYjg4M2NjZjVhOTIiLCJpYXQiOjE3MTE0NzU0ODgsImV4cCI6MTcxMTU2MTg4OH0.Yvil4qLVPXSV7cB5RBhiki7hzqFreQIR8rEUICBqPaU", // Your token here
+      },
+    };
+
+    res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return 401 if token is missing or invalid", async () => {
+    verifyToken.mockResolvedValue(null); // Simulate token verification failure
+
+    await saved_categories(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: "Unauthorized" });
+  });
+
+  it("should return 404 if user is not found", async () => {
+    const payload = { userId: "userId123" };
+    verifyToken.mockResolvedValue(payload); // Simulate token verification success
+    userFindOneMock.mockResolvedValue(null); // Simulate user not found
+
+    await saved_categories(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      success: false,
+      message: "User not found",
+    });
+  });
+});
+
+describe("submit function", () => {
+  let req, res, userFindOneMock, postFindOneMock, userSaveMock, postSaveMock;
+
+  beforeEach(() => {
+    userFindOneMock = jest.spyOn(User, "findOne");
+    userSaveMock = jest.spyOn(User.prototype, "save");
+    postFindOneMock = jest.spyOn(Post, "findOne");
+    postSaveMock = jest.spyOn(Post.prototype, "save");
+
+    req = {
+      headers: {
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjAyM2Q4MDdmNzBkYjg4M2NjZjVhOTIiLCJpYXQiOjE3MTE0NzU0ODgsImV4cCI6MTcxMTU2MTg4OH0.Yvil4qLVPXSV7cB5RBhiki7hzqFreQIR8rEUICBqPaU", // Your token here
+      },
+      body: {
+        postId: "660227d61650ec9f41404c80", // Your post ID here
+      },
+    };
+
+    res = {
+      status: jest.fn(() => res),
+      json: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return 401 if token is missing or invalid", async () => {
+    verifyToken.mockResolvedValue(null); // Simulate token verification failure
+
+    await submit(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: "Unauthorized" });
+  });
+
+  it("should return 404 if user is not found", async () => {
+    const payload = { userId: "userId123" };
+    verifyToken.mockResolvedValue(payload); // Simulate token verification success
+    userFindOneMock.mockResolvedValue(null); // Simulate user not found
+
+    await submit(req, res);
 
     expect(res.status).toHaveBeenCalledWith(404);
     expect(res.json).toHaveBeenCalledWith({
